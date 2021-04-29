043a814dbfe8f421605f73584604e9586ebb76d6
omar kamal hares ahmed <83429939+ok201810@users.noreply.github.com>
2021-04-29 23:52:44 +0200

    Add files via upload

---
+++ omar kamal hares.sh
@@ -0,0 +1,42 @@
+mkdir TEST
+mkdir -p TEST/TEST1
+mkdir -p TEST/TEST2
+
+cd TEST
+cd TEST/TEST1
+cat > file1.txt
+omar kamal hares 2021
+cat > file2.txt
+hello world 
+cat > file3.txt
+hello
+cd .. 
+cd ..
+
+cd TEST
+cd TEST/TEST2
+
+cd ..
+cd ..
+
+cat > okh.txt
+cat > okh.txt
+
+ls a* A*
+okh.txt  okh.txt
+
+ls -1 |  wc -l
+11
+date 
+Thu 29 April 2021 08:20:30 PM PDT 
+ls 
+okh.txt Desktop Downloads pictures Templates Videos 
+okh.txt Documents Music Public 
+pwd 
+/home/ubuntu
+cat > okh.txt
+omar kamal hares
+wc okh.txt
+4 4 31 okh.txt 
+cp /HOME/TEST/TEST1/file1.txt  /HOME/TEST/TEST2
+
